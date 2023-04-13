b8de0aac2e97413201dae1fc07a012d528e63ed9
umQiLe <117270880+umQiLe@users.noreply.github.com>
2023-04-14 02:47:17 +0800

    Update Tester.java
    Alignment of lines

--- Lab/Version1/Lab2 - Generics/L2Q1/Tester.java
+++ Lab/Version1/Lab2 - Generics/L2Q1/Tester.java
@@ -4,7 +4,7 @@ public class Tester {
 	public static void main(String[] args) {
 		StorePairGeneric<Integer> a = new StorePairGeneric<>(6, 4);
 		StorePairGeneric<Integer> b = new StorePairGeneric<>(2, 2);
-        StorePairGeneric<Integer> c = new StorePairGeneric<>(6, 3);
+        	StorePairGeneric<Integer> c = new StorePairGeneric<>(6, 3);
         
         System.out.println(a.compareTo(b));
         System.out.println(a.compareTo(c));
